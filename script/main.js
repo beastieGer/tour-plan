61daeaa80b4128845a8a74aabb88776dd391a067
beastieGer <beastieol@gmail.com>
2022-07-18 17:41:42 +0300

    final version

--- script/main.js
+++ script/main.js
@@ -1,24 +1,51 @@
-const swiper = new Swiper(".hotel-slider", {
+AOS.init();
+
+const swiper = new Swiper('.hotel-slider', {
 	// Optional parameters
 
 	loop: true,
-	effect: "coverflow",
+	effect: 'coverflow',
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".slider-button--next",
-		prevEl: ".slider-button--prev",
+		nextEl: '.slider-button--next',
+		prevEl: '.slider-button--prev',
 	},
 });
 
-const swiperTwo = new Swiper(".reviews-slider", {
+const swiperTwo = new Swiper('.reviews-slider', {
 	// Optional parameters
 
 	loop: true,
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".reviews-button-next",
-		prevEl: ".reviews-button-prev",
+		nextEl: '.reviews-button-next',
+		prevEl: '.reviews-button-prev',
 	},
+});
+
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function () {
+	document.querySelector('.menu').classList.toggle('menu--visible');
+});
+
+const closeBtn = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+const cardpackBtn = document.querySelectorAll('.cardpack-footer__button');
+
+closeBtn.addEventListener('click', () => {
+	modal.classList.toggle('hide');
+});
+
+cardpackBtn.forEach((item) => {
+	item.addEventListener('click', () => {
+		modal.classList.toggle('hide');
+	});
+});
+
+document.addEventListener('keydown', (event) => {
+	if (event.key === 'Escape') {
+		modal.classList.toggle('hide');
+	}
 });
